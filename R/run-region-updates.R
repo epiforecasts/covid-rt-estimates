14f91a02e2420fb32a03df1e97cf6b66c320658a
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-09-02 11:45:51 +0100

    add rlang to stop propagation of warning messages

--- R/run-region-updates.R
+++ R/run-region-updates.R
@@ -5,6 +5,7 @@
 #'
 # Packages
 library(optparse, quietly = TRUE) # bring this in ready for setting up a proper CLI
+library(rlang, quietly = TRUE) # error handling
 
 # Pull in the definition of the regions
 source(here::here("R", "region-list.R"))
@@ -64,6 +65,7 @@ rru_process_locations <- function(regions, args, excludes, includes) {
                                    },
                                    warning = function(w) {
                                      futile.logger::flog.warn("%s: %s - %s", location$name, w$mesage, toString(w$call))
+                                     cnd_muffle(w)
                                    }),
                error = function(e) {
                  futile.logger::flog.error("%s: %s - %s", location$name, e$message, toString(e$call))
@@ -83,6 +85,7 @@ if (sys.nframe() == 0) {
   tryCatch(withCallingHandlers(run_regional_updates(regions = regions, args = args),
                                warning = function(w) {
                                  futile.logger::flog.warn(w)
+                                 cnd_muffle(w)
                                }),
            error = function(e) {
              futile.logger::flog.error(e)
