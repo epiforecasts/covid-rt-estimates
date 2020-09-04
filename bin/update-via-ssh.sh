0b30e26b6d0b1d969f2e1feb801bfaa9c5eda6f4
seabbs <s.e.abbott12@gmail.com>
2020-09-04 11:05:54 +0000

    drop checkout of testing branch from deployment script

--- bin/update-via-ssh.sh
+++ bin/update-via-ssh.sh
@@ -6,7 +6,6 @@ ssh -i $1 $2  'export GITHUB_USERNAME='"'$3'"' && \
   sudo apt-get install -y docker.io && \
   echo "$GITHUB_PASSWORD" | sudo docker login --username $GITHUB_USERNAME --password-stdin docker.pkg.github.com  && \
   if cd covid-rt-estimates; then git pull; else git clone https://github.com/epiforecasts/covid-rt-estimates.git; cd covid-rt-estimates; fi && \
-  git checkout add_regional_estimates && \
   sudo bash bin/update-docker.sh "build" && \
   sudo bash bin/update-via-docker.sh'
   
