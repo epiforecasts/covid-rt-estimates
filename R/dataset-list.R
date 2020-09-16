6ab307836437f0aa435b0d2f53895aeb3f51953b
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-09-16 07:32:15 +0100

    reorder and document

--- R/dataset-list.R
+++ R/dataset-list.R
@@ -1,35 +1,11 @@
 source(here::here("R", "entities.R"))
 
+# This list is processed in order when doing a full run.
+# Order is defined as follows:
+# 1st: Scale (Most broad -> most fine grained) - Currently Regional, National, Subnational
+# 2nd: Alphabetic order
+
 datasets <- c(
-  Region$new(name = "afghanistan",
-             case_modifier = function(cases) {
-               cases <- cases[!is.na(iso_3166_2)]
-               return(cases)
-             },
-             stable = FALSE),
-  Region$new(name = "belgium"),
-  Region$new(name = "brazil"),
-  Region$new(name = "canada"),
-  Region$new(name = "colombia"),
-  Region$new(name = "germany"),
-  Region$new(name = "india"),
-  Region$new(name = "italy"),
-  Region$new(name = "russia"),
-  Region$new(name = "united-kingdom",
-             covid_regional_data_identifier = "UK"),
-  Region$new(name = "united-states",
-             covid_regional_data_identifier = "USA",
-             region_scale = "State"),
-  SuperRegion$new(name = "cases",
-                  case_modifier = function(cases) {
-                    cases <- cases[, region := country]
-                  }),
-  SuperRegion$new(name = "deaths",
-                  case_modifier = function(deaths) {
-                    deaths <- deaths[country != "Cases_on_an_international_conveyance_Japan"]
-                    deaths <- deaths[, cases_new := deaths_new]
-                    deaths <- deaths[, region := country]
-                  }),
   SuperRegion$new(name = "regional-cases",
                   region_scale = "Region",
                   folder_name = "cases",
@@ -53,5 +29,34 @@ datasets <- c(
                                                                          by = c("date")][, region := "Global"]
                     regional_deaths <- data.table::rbindlist(list(regional_deaths, global_deaths),
                                                              fill = TRUE, use.names = TRUE)
-                  })
+                  }),
+  SuperRegion$new(name = "cases",
+                  case_modifier = function(cases) {
+                    cases <- cases[, region := country]
+                  }),
+  SuperRegion$new(name = "deaths",
+                  case_modifier = function(deaths) {
+                    deaths <- deaths[country != "Cases_on_an_international_conveyance_Japan"]
+                    deaths <- deaths[, cases_new := deaths_new]
+                    deaths <- deaths[, region := country]
+                  }),
+  Region$new(name = "afghanistan",
+             case_modifier = function(cases) {
+               cases <- cases[!is.na(iso_3166_2)]
+               return(cases)
+             },
+             stable = FALSE),
+  Region$new(name = "belgium"),
+  Region$new(name = "brazil"),
+  Region$new(name = "canada"),
+  Region$new(name = "colombia"),
+  Region$new(name = "germany"),
+  Region$new(name = "india"),
+  Region$new(name = "italy"),
+  Region$new(name = "russia"),
+  Region$new(name = "united-kingdom",
+             covid_regional_data_identifier = "UK"),
+  Region$new(name = "united-states",
+             covid_regional_data_identifier = "USA",
+             region_scale = "State")
 )
