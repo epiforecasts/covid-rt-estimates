86cd2cadb3c39f1a18f980216ad75555b09844b5
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-10-13 16:43:11 +0100

    default example to demo dataverse

--- data/runtime/example-config.R
+++ data/runtime/example-config.R
@@ -1,5 +1,5 @@
 DATAVERSE_KEY <- "" # API Access Key
-DATAVERSE_SERVER <- "dataverse.harvard.edu" # Which dataverse server to use
-DATAVERSE_VERSEID <- "" # top level dataverse within which data sets should be published
+DATAVERSE_SERVER <- "https://demo.dataverse.org"  # for production: "dataverse.harvard.edu" # Which dataverse server to use
+DATAVERSE_VERSEID <- "covid-rd" # top level dataverse within which data sets should be published
 DATAVERSE_DATA_CONTACT_EMAIL <- "sam.abbott@lshtm.ac.uk"
 DATAVERSE_DATA_CONTACT_NAME <- "Abbot, Sam"
