e5bb995dcea06416eaefd5c6ee3dbfbde545c830
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-10-13 10:29:53 +0100

    No The on MO

--- data/runtime/known-author-affiliations.R
+++ data/runtime/known-author-affiliations.R
@@ -1,3 +1,3 @@
 # used by the publication script to link author email to affiliation
 affiliations <- list(lshtm.ac.uk = "The Centre for Mathematical Modelling of Infectious Diseases at London School of Hygene and Tropical Medicine",
-                     metoffice.gov.uk = "The Met Office")
+                     metoffice.gov.uk = "Met Office")
