0093bc582617ff314416348cfd655a4ea730dde7
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-10-26 17:12:15 +0000

    Break up the individual entities

--- R/entities/publication-metadata.R
+++ R/entities/publication-metadata.R
@@ -1,20 +1,33 @@
-# requirements
-if (!exists("DATAVERSE_COUNTRIES", mode = "function")) source(here::here("R/enumerations", "dataverse-countries.R"))
+#' requirements
+if (!exists("DATAVERSE_COUNTRIES", mode = "function")) source(here::here("R/lists", "dataverse-countries.R"))
 
+#' PublicationMetadata class
 PublicationMetadata <- R6::R6Class("PublicationMetadata",
                                    public = list(
+                                                                        #' @field title String publication title for the dataset
                                      title = "",
+                                                                        #' @field description String long description for the dataset
                                      description = "",
+                                                                        #' @field breakdown_unit String one from ("continent", "country", "region", "state") to specify lowest location type
                                      breakdown_unit = "",
+                                                                        #' @field country String - optional country name (from dataverse-countries) if all the data is within a single country
                                      country = NA,
+                                                                        #' @description
+                                                                        #' Initialize a new instance of PublicationMetadata
+                                                                        #' @param title String publication title for the dataset
+                                                                        #' @param description String long description for the dataset
+                                                                        #' @param breakdown_unit String one from ("continent", "country", "region", "state") to specify lowest location type
+                                                                        #' @param country String - optional country name (from dataverse-countries) if all the data is within a single country
                                      initialize = function(title,
-                                                           description, breakdown_unit, country = NA) {
+                                                           description,
+                                                           breakdown_unit,
+                                                           country = NA) {
                                        self$title <- title
                                        self$description <- description
                                        if (breakdown_unit %in% c("continent", "country", "region", "state")) {
                                          self$breakdown_unit <- breakdown_unit
                                        }else {
-                                         stop("invalid breakdown unit")
+                                         stop(paste("invalid breakdown unit", breakdown_unit))
                                        }
                                        if (is.na(country)) {
                                          if (breakdown_unit %in% c("region", "state")) {
--- R/entities/super-region.R
+++ R/entities/super-region.R
@@ -1,10 +1,29 @@
 #' requirmenets
 if (!exists("AbstractDataset", mode = "function")) source(here::here("R/entities", "abstract-dataset.R"))
 
-# This seemed a better name than global...
+#' SuperRegion class. So called because its bigger than a region and calling it "global" seemed like a programmers idea of a bad joke...
 SuperRegion <- R6::R6Class("SuperRegion",
                            inherit = AbstractDataset,
                            public = list(covid_national_data_identifier = "ecdc",
+
+                                                               #' @description
+                                                               #' Initialise a new `Region` object
+                                                               #' @param name String name of dataset
+                                                               #' @param publication_metadata PublicationMetadata object defining the publication metadata
+                                                               #' @param region_scale string specifying region scale. Currently Region|State|Country
+                                                               #' @param case_modifier lambda for modifying cases. must accept cases as a single parameter and return it
+                                                               #' @param generation_time String, optional override line list file
+                                                               #' @param incubation_period String, optional override line list file
+                                                               #' @param reporting_delay String, optional override line list file
+                                                               #' @param stable Boolean
+                                                               #' @param data_args list for passing to getregionaldata as arguments
+                                                               #' @param truncation Integer Number of days to trim off the end of the newest end of the data
+                                                               #' @param covid_national_data_identifier String specifying identification of data on covid region data
+                                                               #' @param folder_name String, Optional, to specify location of output folder.
+                                                               #' Used to relocate multiple related datasets into a common folder e.g.
+                                                               #' united-kingdom/cases and united-kingdom/deaths - both datasets specify
+                                                               #' folder_name = "united-kingdom"
+                                                               #' @return a new `SuperRegion` object
                                          initialize = function(...,
                                                                covid_national_data_identifier = "ecdc",
                                                                region_scale = "Country",
