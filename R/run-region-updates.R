7f2adec12c9495d475a0686a1460c76e056fc95f
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-11-24 13:04:24 +0000

    remove debug code

--- R/run-region-updates.R
+++ R/run-region-updates.R
@@ -96,7 +96,6 @@ rru_process_locations <- function(datasets, args, excludes, includes) {
                             args$force,
                             args$timeout,
                             refresh = args$refresh)
-          saveRDS(outcome, paste0(location$name, "_raw_outcome.rds"))
         },
           warning = function(w) {
             futile.logger::flog.warn(w)
