f9103c85bc788b2b5cb6be89891d2af5684b098b
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-11-24 14:58:47 +0000

    hotfix: suppress message on git check, update test code

--- R/run-region-updates.R
+++ R/run-region-updates.R
@@ -439,5 +439,5 @@ example_non_cli_trigger <- function() {
   # list is in the format [flag[, value]?,?]+
   args <- rru_cli_interface(c("-w", "-i", "canada/*", "-t", "1800", "-s"))
   setup_log_from_args(args)
-  futile.logger::ftry(run_regional_updates(datasets = datasets, args = args))
+  futile.logger::ftry(run_regional_updates(datasets = DATASETS, derivatives = COLLATED_DERIVATIVES, args = args))
 }
