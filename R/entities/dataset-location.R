0ae42413d7553d81d68c0b436af910f905bb9bd4
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-11-26 09:58:55 +0000

    strip check from the entity due to include loop

--- R/entities/dataset-location.R
+++ R/entities/dataset-location.R
@@ -1,6 +1,4 @@
 #' requirements
-if (!exists("DATASETS", mode = "function")) source(here::here("R/lists", "dataset-list.R"))
-if (!exists("COLLATED_DERIVATIVES", mode = "function")) source(here::here("R/lists", "collated-derivative-list.R"))
 
 #' R6 Class representing a dataset and optionally a sublocation
 DatasetLocation <- R6::R6Class("DatasetLocation",
@@ -15,9 +13,6 @@ DatasetLocation <- R6::R6Class("DatasetLocation",
                                                                 #' @param sublocation String, Optional of sublocation
                                                                 #' @return a new `DatasetLocation` object
                                  initialize = function(dataset, sublocation = NULL) {
-                                   if (!dataset %in% names(DATASETS) && !dataset %in% names(COLLATED_DERIVATIVES)) {
-                                     stop(paste("Dataset id must be in DATASETS - ", dataset))
-                                   }
                                    self$dataset <- dataset
                                    self$sublocation <- sublocation
                                  }
