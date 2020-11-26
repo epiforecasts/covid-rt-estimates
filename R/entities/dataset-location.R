fd4bf68c043c12be7af9a3fc3c2cb455808b20bb
Joe Hickson <joseph.hickson@metoffice.gov.uk>
2020-11-26 09:57:01 +0000

    allow derivatives in the dataset location object

--- R/entities/dataset-location.R
+++ R/entities/dataset-location.R
@@ -1,5 +1,6 @@
 #' requirements
 if (!exists("DATASETS", mode = "function")) source(here::here("R/lists", "dataset-list.R"))
+if (!exists("COLLATED_DERIVATIVES", mode = "function")) source(here::here("R/lists", "collated-derivative-list.R"))
 
 #' R6 Class representing a dataset and optionally a sublocation
 DatasetLocation <- R6::R6Class("DatasetLocation",
@@ -14,7 +15,7 @@ DatasetLocation <- R6::R6Class("DatasetLocation",
                                                                 #' @param sublocation String, Optional of sublocation
                                                                 #' @return a new `DatasetLocation` object
                                  initialize = function(dataset, sublocation = NULL) {
-                                   if (!dataset %in% names(DATASETS)) {
+                                   if (!dataset %in% names(DATASETS) && !dataset %in% names(COLLATED_DERIVATIVES)) {
                                      stop(paste("Dataset id must be in DATASETS - ", dataset))
                                    }
                                    self$dataset <- dataset
